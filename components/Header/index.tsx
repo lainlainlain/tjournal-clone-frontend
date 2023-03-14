15c74396a314c4f404b88dee7436f354cda4088d
layerl <layerlain@yandex.ru>
2023-03-15 01:20:49 +0900

    added components and pages

---
+++ components/Header/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Link from 'next/link';
+import { Paper, Button, IconButton, Avatar } from '@material-ui/core';
+import {
+  SearchOutlined as SearchIcon,
+  CreateOutlined as PenIcon,
+  SmsOutlined as MessageIcon,
+  Menu as MenuIcon,
+  ExpandMoreOutlined as ArrowBottom,
+  NotificationsNoneOutlined as NotificationIcon,
+} from '@material-ui/icons';
+
+import styles from './Header.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <Paper classes={{ root: styles.root }} elevation={0}>
+      <div className="d-flex align-center">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <Link href="/">
+          <img
+            height={35}
+            className="mr-20"
+            src="https://logos-download.com/wp-content/uploads/2019/01/Tjournal_Logo-700x700.png"
+            alt="Logo"
+          />
+        </Link>
+
+        <div className={styles.searchBlock}>
+          <SearchIcon />
+          <input placeholder="Поиск" />
+        </div>
+
+        <Button variant="contained" className={styles.penButton}>
+          Новая запись
+        </Button>
+      </div>
+      <div className="d-flex align-center">
+        <IconButton>
+          <MessageIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationIcon />
+        </IconButton>
+        <Link href="/profile/1" className="d-flex align-center">
+          <Avatar
+            className={styles.avatar}
+            alt="Remy Sharp"
+            src="https://leonardo.osnova.io/5ffeac9a-a0e5-5be6-98af-659bfaabd2a6/-/scale_crop/108x108/-/format/webp/"
+          />
+          <ArrowBottom />
+        </Link>
+      </div>
+    </Paper>
+  );
+};
